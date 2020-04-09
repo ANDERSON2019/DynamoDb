753d2903807cc9e5a2933d380598673a6dbc42d3
Anderson Rocha de Sousa <arsousa@uolinc.com>
2020-04-08 21:22:13 -0300

    DSD

--- main.tf
+++ main.tf
@@ -1,79 +1,19 @@
 resource "aws_dynamodb_table" "sdasdasd" {
   name           = "${var.table_name}"
   billing_mode   = "${var.billing_mode}"
-  read_capacity  = 5
-  write_capacity = 5
+
   hash_key       = "${var.hash_key}"
   range_key      = "${var.range_key}"
 
   attribute {
-    name = "${var.tb1}"
+    name = "${var.hash_key}"
     type = "S"
   }
 
   attribute {
-    name = "${var.tb2}"
+    name = "${var.range_key}"
     type = "S"
   }
-
-    attribute {
-    name = "${var.tb3}"
-    type = "S"
-  }
-
-
-  attribute {
-    name = "${var.tb4}"
-    type = "S"
-  }
-
-
-  attribute {
-    name = "${var.tb5}"
-    type = "S"
-  }
-
-
-  attribute {
-    name = "${var.tb6}"
-    type = "S"
-  }
-
-
-  attribute {
-    name = "${var.tb7}"
-    type = "S"
-  }
-
-
-  attribute {
-    name = "${var.tb8}"
-    type = "S"
-  }
-
-
-  attribute {
-    name = "${var.tb9}"
-    type = "S"
-  }
-
-
-  attribute {
-    name = "${var.tb10}"
-    type = "S"
-  }
-
-
-  attribute {
-    name = "${var.tb11}"
-    type = "S"
-  }
-
-
-  attribute {
-    name = "${var.tb12}"
-    type = "S"
-  }
-
-  tags = "${var.tag}"  
-}
+  
+  tags = "${var.tag}" 
+   }
