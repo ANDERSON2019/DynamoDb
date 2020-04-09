11b1674339f41a0bfdfc12f1264fbe728cf43622
Anderson Rocha de Sousa <arsousa@uolinc.com>
2020-04-08 21:29:28 -0300

    FDAS

--- main.tf
+++ main.tf
@@ -1,7 +1,8 @@
 resource "aws_dynamodb_table" "sdasdasd" {
   name           = "${var.table_name}"
   billing_mode   = "${var.billing_mode}"
-
+  read_capacity  = 5
+  write_capacity = 5
   hash_key       = "${var.hash_key}"
   range_key      = "${var.range_key}"
 
@@ -16,4 +17,5 @@ resource "aws_dynamodb_table" "sdasdasd" {
   }
   
   tags = "${var.tag}" 
+
    }
